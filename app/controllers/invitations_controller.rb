54d57a0bb46a6e1e451e917fe1b2f2ba59c7e5c8
Emily Martinek <emily.martinek12@gmail.com>
2022-04-06 20:34:57 +0200

    render js and open modal

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,11 +1,16 @@
 class InvitationsController < ApplicationController
-  def new
+  def create
     @invitation = Invitation.new(invitation_params)
+
+    respond_to do |format|
+      format.html {}# { redirect_to root_path, notice: 'Invitation was successfully sent.' }
+      format.js { render :new}
+     end
   end
 
   private
 
   def invitation_params
-    params.require(:email, :message)
+    params.require(:email, :message, :cycle_id)
   end
 end
